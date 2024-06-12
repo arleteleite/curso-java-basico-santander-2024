04e8e4c892f03fdb3a89019d97c8a1022d60e8b1
arleteLeite <arlete.leite@gmail.com>
2024-06-12 18:12:41 -0300

    exemplos de estruturas de repedicao e decisao

---
+++ src/edu/arlete/sintaxejava/estruturadedecisao/ResultadoEscolar.java
@@ -0,0 +1,52 @@
+package edu.arlete.sintaxejava.estruturadedecisao;
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 6;
+
+        /*
+         variavel resultado = verifica se a nota é >= 7 se true (aprovado), senão : verifica a segunda condiçaõ
+         se nota é maior ou igual 5 e menor q 7, se true, imprime recuperação, senão : reprovado
+         */
+        System.out.println("#############");
+        System.out.println("ESTRUTURA TERNARIO");
+
+        String resultado = nota >= 7 ? "Aprovado" : nota >=5 && nota < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+
+        System.out.println("#############");
+        System.out.println("ESTRUTURA SWITCH CASE");
+
+        String sigla = "M";
+        switch (sigla){
+            case "P":{
+                System.out.println("Pequeno");
+                break;
+            }
+            case "M":{
+                System.out.println("Médio");
+                break;
+            }
+            case "G":{
+                System.out.println("Grande");
+                break;
+            }
+            default:{
+                System.out.println("Valor indefinido.");
+            }
+        }
+
+        System.out.println("#############");
+        System.out.println("ESTRUTURA IF");
+
+        if (sigla == "P"){
+            System.out.println("Pequeno");
+        } else if (sigla=="M") {
+            System.out.println("Médio");
+        } else if (sigla == "G") {
+            System.out.println("Grande");
+        }else {
+            System.out.println("Valor indefinido.");
+        }
+    }
+}
